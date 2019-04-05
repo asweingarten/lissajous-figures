d803020a7bde9d3c452883fc91113908b726cd19
asweingarten <asweingarten@gmail.com>
2019-04-05 11:21:37 -0700

    onGenericChange

--- src/index.js
+++ src/index.js
@@ -6,23 +6,22 @@ import Konva from 'konva';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.onAChange = this.onAChange.bind(this);
-    this.onBChange = this.onBChange.bind(this);
+    this.onGenericChange = this.onGenericChange.bind(this);
   }
 
   state = {
     A: 200,
-    B: 200
+    B: 200,
+    alpha: 4.0,
+    beta: 2.0
+  }
+
+  onGenericChange(prop) {
+    return (event) => {
+      this.setState({[prop]: event.target.value});
+    }
   }
   
-  onAChange(newA) {
-    this.setState({A: newA.target.value});
-  }
-
-  onBChange(newB) {
-    this.setState({B: newB.target.value});
-  }
-
   componentDidMount() {
     // this.interval = setInterval(() => this.setState({ time: Date.now() }), 500);
   }
@@ -31,40 +30,44 @@ class App extends Component {
   }
 
   render() {
-    let {A, B} = this.state;
-    let alpha = 4.0;
-    let beta = 2.0;
+    let {A, B, alpha, beta} = this.state;
     let delta = Math.PI/2;
 
     let X = (A, alpha, delta) => (t) => A * Math.sin (alpha * t + delta);
     let Y = (B, beta) => (t) => B * Math.sin (beta * t);
 
-
     let xgo = X (A, alpha, delta);
     let ygo = Y (B, beta);
-    console.log(`xgo: ${xgo(5)}      ygo: ${ygo(5)}`);
-    const width = 200;
-    const height = 200;
-    return (
-      <div>
-        <label>A:</label><input type="number" onChange={this.onAChange} value={A} ></input>
-        <label>B:</label><input type="number" onChange={this.onBChange} value={B} ></input>
-        <Stage width={window.innerWidth} height={window.innerHeight} fill='#fffbf4'>
-          <Layer fill="#fffbf4"
-                x={window.innerWidth/2 - width/2}
-                y={window.innerHeight/2 - height/2}
-                width={width}
-                height={height}>
-            {[...Array(5000)].map((x,i) => (
-              <Rect
+
+    let path = [];
+    const pixels = [...Array(5000)].map(
+      (x, i) => {
+              return <Rect
                 x={xgo(i)}
                 y={ygo(i)}
                 width={2}
                 height={2}
                 fill='black'
               />
-            ))}
-            </Layer>
+    });
+      // path.push ({x: , y: });
+    // console.log(`xgo: ${xgo(5)}      ygo: ${ygo(5)}`);
+    const width = 200;
+    const height = 200;
+    return (
+      <div>
+        <label>A:</label><input type="number" onChange={this.onGenericChange("A")} value={A} ></input>
+        <label>B:</label><input type="number" onChange={this.onGenericChange("B")} value={B} ></input>
+        <label>alpha:</label><input type="number" onChange={this.onGenericChange("alpha")} value={alpha} ></input>
+        <label>beta:</label><input type="number" onChange={this.onGenericChange("beta")} value={beta} ></input>
+        <Stage width={window.innerWidth} height={window.innerHeight} fill='#fffbf4'>
+          <Layer fill="#fffbf4"
+                x={window.innerWidth/2 - width/2}
+                y={window.innerHeight/2 - height/2}
+                width={width}
+                height={height}>
+            {pixels}
+          </Layer>
         </Stage>
       </div>
     );
